c8cffdbc3a3e3c81a02826c151609792355d2704
Axel Torrico <torricohernan2@gmail.com>
2023-05-30 17:06:07 -0300

    añadí el componente ScrollDown

---
+++ src/components/ScrollDown.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import "../styles/ScrollDown.css";
+
+export default function ScrollDown() {
+  const [scroll, setScroll] = useState(false);
+
+  const fadeScroll = () => {
+    if (window.scrollY >= 150) {
+      setScroll(true);
+    } else {
+      setScroll(false);
+    }
+  };
+
+  window.addEventListener("scroll", fadeScroll);
+
+  return (
+    <div
+      className={
+        scroll ? "button-container scroll-inactive" : "button-container "
+      }
+    >
+      <div className="scroll-button"></div>
+      <h6>Scroll</h6>
+    </div>
+  );
+}
