28128bc0ceffca232804395e569a3339887b5589
Axel Torrico <torricohernan2@gmail.com>
2023-06-02 12:36:25 -0300

    agregue funcionalidad al componente

--- src/components/UpButton.jsx
+++ src/components/UpButton.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/UpButton.css'
 
 //icon
 import { IoIosArrowUp } from 'react-icons/io'
 
 export function UpButton() {
+  const [show, setShow] = useState(false)
+
+  const appearButton = () => {
+    window.scrollY >= 500 ? setShow(true) : setShow(false)
+  }
+
+  window.addEventListener('scroll', appearButton)
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+
   return (
-    <button className='UpButton'>
+    <button className={show ? 'UpButton' : 'UpButton-inactive'} onClick={scrollToTop}>
       <IoIosArrowUp className='UpButton-icon' />
     </button>
   );
