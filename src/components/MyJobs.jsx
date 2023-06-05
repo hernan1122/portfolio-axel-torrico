623d06695a32b87dee302c3902321a2ed9f3ede2
Axel Torrico <torricohernan2@gmail.com>
2023-06-05 11:05:16 -0300

    pase la seccion de jobs de App a MyJobs

--- src/components/MyJobs.jsx
+++ src/components/MyJobs.jsx
@@ -1,19 +1,148 @@
+import React from 'react';
+import { WorkCard } from './workCard';
 import '../styles/MyJobs.css'
+
+//images
+import todoList from '../images/todo-list.png'
+import moch from '../images/moch-movies.png'
+import criptar from '../images/criptar.png'
+import batatabit from '../images/Batatabit.png'
+import chronometer from '../images/my-chronometer.png'
+import picturesDogs from '../images/fotos de perros.png'
+import gameZombi from '../images/game-zombi.png'
+import gameTateti from '../images/ta-te-ti.png'
+import interactiveGiveaway from '../images/sorteo-interactivo.png'
+
+//icons
+import { AiOutlineGithub } from 'react-icons/ai'
+import { GoTools } from 'react-icons/go'
+
+const jobs = [
+  {
+    image: `${todoList}`,
+    title: 'ToDo List',
+    info: 'HTML-CSS-JS-REACT',
+    description: 'With ToDo List you can organize your day without any problem, start using it!!',
+    urlGit: 'https://github.com/hernan1122/react-proyecto-todo',
+    urlApp: 'https://react-proyecto-todo-6q38rx77q-hernan1122.vercel.app/'
+  },
+  {
+    image: `${moch}`,
+    title: 'Moch',
+    info: 'HTML-CSS-JS',
+    description: 'Moch shows you the trending movies of the moment! Browse through it in different categories and create your list of favorite movies.',
+    urlGit: 'https://github.com/hernan1122/moch-movie',
+    urlApp: 'https://moch-movie.vercel.app/'
+  },
+  {
+    image: `${criptar}`,
+    title: 'CriptAR',
+    info: 'HTML-CSS-JS',
+    description: 'In this app you can see the price of the dollar and different cryptocurrencies in national banks and exchanges.',
+    urlGit: 'https://github.com/hernan1122/utilizando-api-criptoya',
+    urlApp: 'https://utilizando-api-criptoya.vercel.app/'
+  },
+  {
+    image: `${picturesDogs}`,
+    title: 'Pictures of Dogs',
+    info: 'HTML-CSS-JS',
+    description: 'In this application you will see photos of dogs randomly, and if you like one you can add it to your list to see it whenever you want. You can also upload the photo of your puppy.',
+    urlGit: 'https://github.com/hernan1122/fotos-de-perros',
+    urlApp: 'https://fotos-de-perros.vercel.app/'
+  },
+  {
+    image: `${chronometer}`,
+    title: 'Chronometer',
+    info: 'HTML-CSS-JS',
+    description: 'Use this digital stopwatch to take your time when you go for a run or in competitions with your friends.',
+    urlGit: 'https://github.com/hernan1122/cronometro-con-javascript',
+    urlApp: 'https://cronometro-con-javascript.vercel.app/'
+  },
+  {
+    image: `${gameZombi}`,
+    title: 'Game "Zombi"',
+    info: 'HTML-CSS-JS',
+    description: 'Can you complete all the levels of this game in record time? find it out!!',
+    urlGit: 'https://github.com/hernan1122/practico-javascript-videogames',
+    urlApp: 'https://practico-javascript-videogames-3fyn.vercel.app/'
+  },
+  {
+    image: `${gameTateti}`,
+    title: 'Game "TA TE TI"',
+    info: 'HTML-CSS-JS-REACT',
+    description: 'The traditional game of "ta te ti" now available on your cell phone.',
+    urlGit: 'https://github.com/hernan1122/tic-tac-toe',
+    urlApp: 'https://tic-tac-toe-pearl-theta.vercel.app/'
+  },
+  {
+    image: `${batatabit}`,
+    title: 'Batatabit',
+    info: 'HTML-CSS',
+    description: 'Batatabit is the next revolution in cryptocurrency exchange!! In it you will have information in real time about the rates of change that determine the behavior.',
+    urlGit: 'https://github.com/hernan1122/Batatabit',
+    urlApp: 'https://batatabit-ecru.vercel.app/'
+  },
+  {
+    image: `${interactiveGiveaway}`,
+    title: 'Interactive Giveaway',
+    info: 'HTML-CSS-JS',
+    description: 'If you want to run a draw quickly and make it reliable, use this app, which is perfect for that.',
+    urlGit: 'https://github.com/hernan1122/sorteo-interactivo',
+    urlApp: 'https://sorteo-interactivo.vercel.app/'
+  },
+]
+
+export function MyJobs() {
+  return (
+    <div className='MyJobs'>
+      <h2 className="MyJobs-title">
+        <GoTools className='MyJobs-icon'/> My Jobs
+      </h2>
+      <div className='MyJobs-container-btn'>
+        <a
+          className='MyJobs-portfolio-repository'
+          href='https://github.com/hernan1122/portfolio-axel-torrico'
+          target="_blank"
+          data-aos="zoom-in"
+        >
+          <i><AiOutlineGithub /></i> Portfolio repository
+        </a>
+      </div>
+      <div className='MyJobs-cards'>
+        {
+          jobs.map(({ image, title, info, description, urlGit, urlApp }) => {
+            return (
+              <WorkCard
+                key={title}
+                image={image}
+                title={title}
+                info={info}
+                description={description}
+                urlGit={urlGit}
+                urlApp={urlApp}
+              >
+              </WorkCard>
+            )
+          })
+        }
+      </div>
+    </div>
+  );
+}
+
+
+
+
+
+/* import '../styles/MyJobs.css'
 
 //icons
 import { AiOutlineGithub } from 'react-icons/ai'
 import { RiEyeLine } from 'react-icons/ri'
 
-/* import AOS from 'aos';
-import 'aos/dist/aos.css';
-AOS.init({
-  duration: 100,
-  once: true,
-}); */
-
 export function MyJobs({ image, title, info, description, urlGit, urlApp }) {
   return (
-    <div /* data-aos="zoom-in" */>
+    <div>
       <article id="jobs" className="jb-container">
         <img className="jb-container-image" src={image} alt={image} />
         <div className="jb-container-description">
@@ -47,47 +176,4 @@ export function MyJobs({ image, title, info, description, urlGit, urlApp }) {
     </div>
   )
 }
-
-
-
-/* export function MyJobs({ image, title, info, urlGit, urlApp, seeMore }) {
-  return (
-    <div data-aos="zoom-in">
-      <article id="jobs" className="jb-container">
-        <img className="jb-container-image" src={image} alt={image} />
-        <div className="jb-container-link">
-          <div className="jb-container-link-content">
-            <div className="jb-container-link-content-title">
-              <h3>{ title }</h3>
-              <p>{ info }</p>
-            </div>
-          </div>
-          <div className="jb-container-link-content-icon">
-              <a 
-              className="jb-container-icon" 
-              href={urlGit} target="_blank"
-              >
-                <i>
-                  <AiOutlineGithub />
-                </i>
-              </a>
-              <a 
-              className="jb-container-icon" 
-              href={urlApp} target="_blank"
-              >
-                <i>
-                  <RiEyeLine />
-                </i>
-              </a>
-            <button
-              className='bnt-see-more'
-              onClick={seeMore}
-            >
-                See more
-              </button>
-            </div>
-        </div>
-      </article>
-    </div>
-  )
-} */
+ */
