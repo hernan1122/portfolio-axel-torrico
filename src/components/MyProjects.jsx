b1c292869f71ba6689adfd77431c55c728ec0ad3
Axel Torrico <torricohernan2@gmail.com>
2023-06-06 16:13:57 -0300

    cambie de lugar el proyecto y el boton, ademas cree otro boton que nos lleva a MyJobs

--- src/components/MyProjects.jsx
+++ src/components/MyProjects.jsx
@@ -1,28 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Projects } from './Projects';
 import '../styles/MyProjects.css'
 
 //icons
 import { AiOutlineGithub } from 'react-icons/ai'
+import { BsFileCodeFill } from 'react-icons/bs'
 import { GoTools } from 'react-icons/go'
 
 export function MyProjects() {
   return (
-    <div className='MyProjects'>
+    <div id="myprojects" className='MyProjects'>
       <h2 className="MyProjects-title">
-        <GoTools className='MyProjects-icon'/> My Jobs
+        <BsFileCodeFill className='MyProjects-icon'/> My Projects
       </h2>
-      <div className='MyProjects-container-btn'>
-        <a
-          className='MyProjects-portfolio-repository'
-          href='https://github.com/hernan1122/portfolio-axel-torrico'
-          target="_blank"
-          data-aos="zoom-in"
-        >
-          <i><AiOutlineGithub /></i> Portfolio repository
-        </a>
+      <div className='MyProjects-container-projects'>
+        <Projects />
       </div>
-      <Projects />
+      <div className='MyProjects-container-button'>
+        <div className='MyProjects-container-btn'>
+          <a
+            className='MyProjects-portfolio-repository'
+            href='https://github.com/hernan1122/portfolio-axel-torrico'
+            target="_blank"
+            data-aos="zoom-in"
+          >
+            <i><AiOutlineGithub /></i> Portfolio Repository
+          </a>
+        </div>
+        <div className='MyProjects-container-btn'>
+          <Link
+            to={'/jobs'}
+            className='MyProjects-portfolio-repository'
+            data-aos="zoom-in"
+          >
+            <i><GoTools /></i> More Jobs
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
