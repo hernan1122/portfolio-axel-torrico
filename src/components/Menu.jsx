84d20751c9301774e9e017d750815fbe96a0e172
Axel Torrico <torricohernan2@gmail.com>
2023-06-07 10:05:44 -0300

    elimine home y my jobs

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -36,19 +36,9 @@ export function Menu() {
       <div className='menu-links' /* ref={menuRef} */>
         <ul className={`ul ${isOpen ? 'open' : ''}`}>
           <li>
-            <Link to={'/'} onClick={handleClick}>
-              <AiFillHome className='menu-icon'/> Home
-            </Link>
-          </li>
-          <li>
             <a id='aboutMe' href="#about" onClick={handleClick}>
               <BsFillPersonFill className='menu-icon'/> About me
             </a>
-          </li>
-          <li>
-            <Link to={'/jobs'} onClick={handleClick}>
-              <GoTools className='menu-icon' /> My Jobs
-            </Link>
           </li>
           <li>
             <a id='myProjects' href="#myprojects" onClick={handleClick}>
@@ -78,3 +68,52 @@ export function Menu() {
     </nav>
   )
 }
+
+
+// return (
+//     <nav className='menu-container'>
+//       <a className='menu-title' href='#home'>Portfolio | <span>Axel Torrico</span></a>
+//       <div className='menu-links' ref={menuRef}>
+//         <ul className={`ul ${isOpen ? 'open' : ''}`}>
+//           <li>
+//             <Link to={'/'} onClick={handleClick}>
+//               <AiFillHome className='menu-icon'/> Home
+//             </Link>
+//           </li>
+//           <li>
+//             <a id='aboutMe' href="#about" onClick={handleClick}>
+//               <BsFillPersonFill className='menu-icon'/> About me
+//             </a>
+//           </li>
+//           <li>
+//             <Link to={'/jobs'} onClick={handleClick}>
+//               <GoTools className='menu-icon' /> My Jobs
+//             </Link>
+//           </li>
+//           <li>
+//             <a id='myProjects' href="#myprojects" onClick={handleClick}>
+//               <BsFileCodeFill className='menu-icon' /> My Projects
+//             </a>
+//           </li>
+//           <li>
+//             <a id='contactMe' href="#contacts" onClick={handleClick}>
+//               <AiFillPhone className='menu-icon' /> Contact me
+//             </a>
+//           </li>
+//           <li>
+//             <a id='cv' className="cv" download='CV Axel Torrico' href={cv} onClick={handleClick}>
+//               <BsFilePersonFill className='menu-icon' /> C. Vitae
+//             </a>
+//           </li>
+//         </ul>
+//       </div>
+//       <div
+//         onClick={handleClick}
+//         className={`icon nav-icon-5 ${isOpen && 'open'}`}
+//       >
+//         <span></span>
+//         <span></span>
+//         <span></span>
+//       </div>
+//     </nav>
+//   )
