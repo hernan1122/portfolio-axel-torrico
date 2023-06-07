d8064c310b4abbb82145aa25a3fea8f9128aa4ba
Axel Torrico <torricohernan2@gmail.com>
2023-06-07 10:06:07 -0300

    elimine la ruta de jobs

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from '../Home'
-import { MyJobs } from '../components/MyJobs'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path="/jobs" element={<MyJobs />} />
       </Routes>
     </BrowserRouter>
   );
