0dd290b3d795e5b66ba576e9192a3d48dce7ad55
Axel Torrico <torricohernan2@gmail.com>
2023-06-07 10:58:15 -0300

    llame a Preloader a App

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from '../Home'
+import { Preloader } from '../components/Preloader';
 
 function App() {
+  const [start, setStart] = useState(true)
+
+  setTimeout(() => { setStart(false) }, 3000)
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
+      {start ? <Preloader /> :
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      }
     </BrowserRouter>
   );
 }
