6e908346182a6b74ba12114e51dd4c748a181041
axel torrico <torricohernan2@gmail.com>
2023-06-25 21:54:50 -0300

    llame a projects.js y modifique estilos

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,14 +1,8 @@
 import React, { useState } from 'react';
 import AOS from 'aos';
+import project from '../json/projects'
 import { ProjectsSlider } from './ProjectsSlider';
 import '../styles/Projects.css'
-
-//images and video
-import home from '../images/cantera-music-home.jpg'
-import search from '../images/cantera-music-search.png'
-import videoPoster from '../images/cantera-music-initial.png'
-import video from '../video/video-cantera-music.mp4'
-
 
 export function Projects() {
   AOS.init()
@@ -19,44 +13,48 @@ export function Projects() {
   }
 
   return (
-    <div className='Projects' data-aos="zoom-in">
-      <div className='Projects-container'>
-        <div>
-          <h3>Cantera Music <span>Explore, enjoy and share</span></h3>
-          <p>
-            A music application that offers users a comprehensive and personalized audio experience. With its ability to play music, podcasts, and audiobooks, as well as add custom audio files, the app becomes a versatile platform to meet users' listening needs.
-          </p>
-          <p>
-            As a member of the frontend team, I was responsible for UI implementation using React, API integration, component styling, and interaction logic implementation.
-          </p>
-        </div>
-        <video
-          className='Projects-video'
-          poster={videoPoster}
-          controls
-        >
-          <source src={video} type="video/mp4" />
-          Tu navegador no admite el elemento de video.
-        </video>
-      </div>
-      <div className='Projects-container-img'>
-        <img src={home} alt={home} />
-        <img src={search} alt={search} />
-      </div>
-      <a
-        href="https://cantera-music.vercel.app/"
-        target="_blank"
-        className='Projects-container-btn-see'
-      >
-        See App
-      </a>
-      <button className='Projects-container-btn' onClick={handleClick}>
-        {seeImagen ? 'Back' : 'See images'}
-      </button>
-      {seeImagen &&
-        <div className='Projects-container-slider' >
-          <ProjectsSlider />
-        </div>
+    <div className='Projects'>
+      {
+        project.map((project) => {
+          return (
+            <div className='Projects-container' data-aos="zoom-in">
+              <div key={project.title} className='Projects-content'>
+                <div>
+                  <h3>{project.title} <span>{project.skills}</span></h3>
+                  <p>{project.description_one}</p>
+                  <p>{project.description_two}</p>
+                </div>
+                <video
+                  className='Projects-video'
+                  poster={project.videoPoster}
+                  controls
+                >
+                  <source src={project.video} type="video/mp4" />
+                  Tu navegador no admite el elemento de video.
+                </video>
+              </div>
+              <div className='Projects-content-img'>
+                <img src={project.imageOne} alt={project.title} />
+                <img src={project.imageTwo} alt={project.title} />
+              </div>
+              <a
+                href={project.urlApp}
+                target="_blank"
+                className='Projects-content-btn-see'
+              >
+                See App
+              </a>
+              <button className='Projects-content-btn' onClick={handleClick}>
+                {seeImagen ? 'Back' : 'See images'}
+              </button>
+              {seeImagen &&
+                <div className='Projects-content-slider' >
+                  <ProjectsSlider />
+                </div>
+              }
+            </div>
+          )
+        })
       }
     </div>
   );
